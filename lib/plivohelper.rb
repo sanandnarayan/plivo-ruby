b53e60c3ca2513263c47bc40af1eb14ed387d801
bevenky <venky@miglu.com>
2011-06-22 16:51:42 +0530

    Add record to conference, change format to fileFormat

--- lib/plivohelper.rb
+++ lib/plivohelper.rb
@@ -336,7 +336,7 @@ module Plivo
   class Record
     extend Plivo::Element::ClassMethods
     include Plivo::Element
-    attributes :timeout, :finishOnKey, :maxLength, :playBeep, :format, :prefix, :filePath, :bothLegs
+    attributes :timeout, :finishOnKey, :maxLength, :playBeep, :fileFormat, :prefix, :filePath, :bothLegs
   end
 
   class Dial
@@ -373,7 +373,7 @@ module Plivo
   class Conference
     extend Plivo::Element::ClassMethods
     include Plivo::Element
-    attributes :muted, :enterSound, :exitSound, :startConferenceOnEnter, :endConferenceOnExit, :waitSound, :timeLimit, :hangupOnStar
+    attributes :muted, :enterSound, :exitSound, :startConferenceOnEnter, :endConferenceOnExit, :waitSound, :timeLimit, :hangupOnStar, :recordFilePath, :recordFileFormat, :recordFilePrefix
   end
 
   class PreAnswer
